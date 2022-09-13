b235fd64396dfe279c381c5ca3b62d2480b5152d
Gan Quan <imganquan@gmail.com>
2022-09-13 11:47:33 +0800

    example of color
    example of color

--- src/fmt_test.cpp
+++ src/fmt_test.cpp
@@ -1,14 +1,15 @@
-#include <fmt/core.h>
 #include <fmt/color.h>
+#include <fmt/core.h>
 
 int main() {
+    fmt::print("Hello, world!\n");
 
-  fmt::print("Hello, world!\n");
+    for (int i = 0; i < 3; i++) {
+        fmt::print("i = {}\n", i);
+    }
 
-  for(int i = 0; i < 3; i++) {
-    fmt::print("i = {}\n", i);
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Hello, {}!\n",
+               "world");
 
-  }
-
-  return 0;
+    return 0;
 }
