8f1118e1c023f5cc95d9fc3a31ac035c1449e077
Gan Quan <imganquan@gmail.com>
2022-09-16 13:19:32 +0800

    fmt is awesome!!!

--- src/fmt_test.cpp
+++ src/fmt_test.cpp
@@ -1,5 +1,9 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
+
+#include <vector>
+#include <map>
 
 int main() {
     fmt::print("Hello, world!\n");
@@ -10,6 +14,12 @@ int main() {
 
     fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Hello, {}!\n",
                "world");
+
+    std::vector<int> vi{1, 2, 3};
+    fmt::print("vi = {}\n", vi);
+
+    std::map<int, int> m{{1,2}, {2,3}, {3,4}};
+    fmt::print("m = {}\n", m);
 
     return 0;
 }
